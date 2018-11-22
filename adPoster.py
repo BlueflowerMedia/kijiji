dbc21c8766f1cc1ab55d7d86a79c1f10de4ff557
fahim shahreer <fahim.shahreer95@gmail.com>
2018-11-21 20:05:39 -0500

    old version

---
+++ adPoster.py
@@ -0,0 +1,33 @@
+from subprocess import Popen, PIPE
+from subprocess import call
+
+# Writes logs to log.txt
+def logger(log):
+    with open("log.txt", "a") as myfile:
+        myfile.write(log)
+        print(log)
+
+# main function
+def adPoster(job_file, repost=True):
+    # call(["python", "kijiji_repost_headless", "repost", job_file])
+    if (repost==True):
+        p = Popen(["python", "kijiji_repost_headless", "repost", job_file], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        # p = Popen(["echo","repost"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    else:
+        p = Popen(["python", "kijiji_repost_headless", "post", job_file], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        # p = Popen(["echo","post"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    rc = p.returncode
+
+    # logging stdout
+    logger("ADPOSTER OUTPUTS:")
+    for line in output.decode("ascii").split('\n'):
+        logger(line)
+
+    # logging stderr
+    logger("ADPOSTER ERROR: ")
+    for line in err.decode("ascii").split('\n'):
+        logger(line)
+
+    return rc
