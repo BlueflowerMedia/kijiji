2af76ff76b9a882ed89219dff68befa95588f5e8
fahim shahreer <fahim.shahreer95@gmail.com>
2018-11-21 20:03:58 -0500

    brand new changes

---
+++ adScheduler.py
@@ -0,0 +1,41 @@
+import schedule
+import time
+import datetime
+from adPoster import *
+
+class adSchedule:
+
+    def __init__(self, job_id, repeat, delay=0):
+        self.job_id = job_id
+        self.repeat = repeat
+        self.delay = delay
+        self.job_file = "ad" + str(self.job_id) + "/ad" + str(self.job_id) + ".yml"
+
+    def printHeader(self):
+        logger(str(datetime.datetime.now()))
+        logger("AD#" + str(self.job_id) + " is being reposted every " + str(self.repeat) + " minutes\n")
+        logger("AD FILE: " + str(self.job_file) + "\n")
+
+    def printFooter(self, rc):
+        if (rc==0):
+            logger("No critical errors. Check ADPOSTER OUTPUTS for details")
+            logger("=====================================================")
+        else:
+            logger("There was an error. Check ADPOSTER ERROR for details")
+            logger("=====================================================")
+
+    def start(self):
+        # delay
+        time.sleep(self.delay*60)
+        self.printHeader()
+        # running the ad first time
+        rc = adPoster(self.job_file, repost=False)
+        self.printFooter(rc)
+        #schedule
+        schedule.every(self.repeat).minutes.do(self.repost)
+
+    def repost(self):
+        self.printHeader()
+        # reposting because repost=True by default [check adPoster]
+        rc = adPoster(self.job_file)
+        self.printFooter(rc)
---
+++ schedule1.py
@@ -0,0 +1,11 @@
+from adScheduler import *
+
+# Setting the schedule settings
+ad1 = adSchedule(job_id=1, repeat=5)
+ad1.start()
+
+ad2 = adSchedule(job_id=2, repeat=3, delay=5)
+ad2.start()
+
+while True:
+    schedule.run_pending()
